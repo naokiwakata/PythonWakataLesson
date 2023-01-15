3e1f3ccd6687d107caf07a7a29a1e78f7a86fdb2
naokiwakata <rockwakata15@gmail.com>
2023-01-15 20:37:29 +0900

    白黒課題

---
+++ src/opencv/g.py
@@ -0,0 +1,20 @@
+import cv2
+
+img_path = "image//gakky.png"
+
+img = cv2.imread(img_path)
+
+# 高さ、幅、チャンネルを取得
+height, width, channel = img.shape
+
+threshold = 120  # しきい値（自分で設定）
+
+for x in range(width):
+    for y in range(height):
+        if img[x, y][0] > threshold:  # B(RGBのうち)の値がしきい値より大きい場合
+            img[x, y] = [0, 0, 0]  # 黒
+        else:
+            img[x, y] = [255, 255, 255]  # 白
+
+cv2.imshow('shirokuro', img)
+cv2.waitKey(0)
