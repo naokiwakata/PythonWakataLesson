fc8cd74a929c45afb268a99e07afb3270124e206
naokiwakata <rockwakata15@gmail.com>
2023-01-16 17:02:04 +0900

    高さ逆

--- src/opencv/h.py
+++ src/opencv/h.py
@@ -5,7 +5,7 @@ img_path = "image//gakky.png"
 img = cv2.imread(img_path)
 
 # 高さ、幅、チャンネルを取得
-height, width, channel = img.shape
+width, height, channel = img.shape
 
 threshold = 120  # しきい値（自分で設定）
 
